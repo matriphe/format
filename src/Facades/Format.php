87130d06975c738cabf28a0b9e8767becd68d0ce
Muhammad Zamroni <zam@nuwira.com>
2015-01-31 23:03:05 +0700

    Add Service Provider and Facades for Laravel

---
+++ src/Facades/Format.php
@@ -0,0 +1,9 @@
+<?php namespace Matriphe\Format\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Format extends Facade {
+
+    protected static function getFacadeAccessor() { return 'format'; }
+
+}
---
+++ src/FormatServiceProvider.php
@@ -0,0 +1,22 @@
+<?php namespace Matriphe\Format;
+
+use Illuminate\Support\ServiceProvider;
+
+class FormatServiceProvider extends ServiceProvider {
+
+    protected $defer = false;
+
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        $this->app['format'] = $this->app->share(function($app)
+        {
+            return new Format;
+        });
+    }
+
+}
