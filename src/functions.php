679717d2d754876dc3ed27a1cae28acd77f600d7
Muhammad Zamroni <halo@matriphe.com>
2015-10-15 15:58:15 +0700

    More test to figure out how it's worked

--- src/functions.php
+++ src/functions.php
@@ -1,8 +1,10 @@
 <?php
 
+use Matriphe\Format;
+
 if (! function_exists('format_number')) {
     function format_number($num, $sep = 0, $decimal = ',', $thousand = '.')
     {
-        return $num;//app('Format')->number($num, $sep, $decimal, $thousand);
+        return app('Format')->number($num, $sep, $decimal, $thousand);
     }
 }
